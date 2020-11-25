2d5d7162d39f411c210b36286529f8f8438da4bc
Maurici Armora <maurici.armora@gmail.com>
2020-11-25 16:27:37 +0100

    Reestructured javascript files

---
+++ javascript/slide-in.js
@@ -0,0 +1,29 @@
+export const slideIn = document.querySelectorAll('.slide-in');
+
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function() {
+    let context = this, args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide(e) {
+  slideIn.forEach(slider => {
+    const slideInAt = (window.scrollY + window.innerHeight) - slider.clientHeight / 3
+
+    if (slideInAt > slider.offsetTop) {
+      console.log(slider);
+      slider.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
