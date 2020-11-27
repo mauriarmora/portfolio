6c4e1523c08b069eed7c71e4d19fdce9e54e00e6
Maurici Armora <maurici.armora@gmail.com>
2020-11-27 17:33:29 +0100

    Making it responsive

--- javascript/banner.js
+++ javascript/banner.js
@@ -1,22 +1,22 @@
-export const banner = document.querySelector('.presentation');
+// export const banner = document.querySelector('.presentation');
 
-function makeSmaller(e) {
-  const bannerText = e.currentTarget.querySelectorAll('h1, h4');
-  e.currentTarget.style.transition = '0.4s';
-  e.currentTarget.style.transform = 'scale(0.9)';
-}
+// function makeSmaller(e) {
+//   const bannerText = e.currentTarget.querySelectorAll('h1, h4');
+//   e.currentTarget.style.transition = '0.4s';
+//   e.currentTarget.style.transform = 'scale(0.9)';
+// }
 
-function makeBigger(e) {
-  if (e.currentTarget.style.transform === 'scale(1)') {return}
-  e.currentTarget.style.transition = 'all 0.5s cubic-bezier(0.25, 0.5, 0.3, 1.7) 0s';
-  e.currentTarget.style.transform = 'scale(1.2)';
-}
+// function makeBigger(e) {
+//   if (e.currentTarget.style.transform === 'scale(1)') {return}
+//   e.currentTarget.style.transition = 'all 0.5s cubic-bezier(0.25, 0.5, 0.3, 2) 0s';
+//   e.currentTarget.style.transform = 'scale(1.2)';
+// }
 
-function originalSize(e) {
-  e.currentTarget.style.transition = '0.7s';
-  e.currentTarget.style.transform = 'scale(1)';
-}
+// function originalSize(e) {
+//   e.currentTarget.style.transition = '0.7s';
+//   e.currentTarget.style.transform = 'scale(1)';
+// }
 
-banner.addEventListener('mouseenter', makeSmaller);
-banner.addEventListener('transitionend', makeBigger);
-banner.addEventListener('mouseleave', originalSize);
+// banner.addEventListener('mouseenter', makeSmaller);
+// banner.addEventListener('transitionend', makeBigger);
+// banner.addEventListener('mouseleave', originalSize);
--- javascript/slide-in.js
+++ javascript/slide-in.js
@@ -1,29 +1,29 @@
-export const slideIn = document.querySelectorAll('.slide-in');
+// export const slideIn = document.querySelectorAll('.slide-in');
 
-function debounce(func, wait = 15, immediate = true) {
-  let timeout;
-  return function() {
-    let context = this, args = arguments;
-    let later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    let callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+// function debounce(func, wait = 15, immediate = true) {
+//   let timeout;
+//   return function() {
+//     let context = this, args = arguments;
+//     let later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     let callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
 
-function checkSlide(e) {
-  slideIn.forEach(slider => {
-    const slideInAt = (window.scrollY + window.innerHeight) - slider.clientHeight / 3
+// function checkSlide(e) {
+//   slideIn.forEach(slider => {
+//     const slideInAt = (window.scrollY + window.innerHeight) - slider.clientHeight / 3
 
-    if (slideInAt > slider.offsetTop) {
-      console.log(slider);
-      slider.classList.add('active');
-    }
-  });
-}
+//     if (slideInAt > slider.offsetTop) {
+//       console.log(slider);
+//       slider.classList.add('active');
+//     }
+//   });
+// }
 
-window.addEventListener('scroll', debounce(checkSlide));
+// window.addEventListener('scroll', debounce(checkSlide));
